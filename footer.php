66470060f986c40900a69d2105bda16d140516b5
icedmuffin <65304518+icedmuffin@users.noreply.github.com>
2021-10-07 19:28:01 +0700

    migrasi file

---
+++ footer.php
@@ -0,0 +1,59 @@
+<footer class="container-fluid footer">
+    <section class="informasi">
+        <section class="navigation">
+            <section class="sub-nav">
+                <h3>Keprofesian</h3>
+
+                <a href="#">Registrasi Keanggotaan</a>
+                <a href="#">Daftar Keanggotaan</a>
+                <a href="#">STR</a>
+                <a href="#">Agenda Akan Dilakukan</a>
+                <a href="#">Agenda Telah Dilakukan</a>
+                <a href="#">Arsip Dokumen</a>
+            </section>
+
+            <section class="sub-nav">
+                <h3>Kemitraan Unicef</h3>
+                <a href="#">Imunisasi</a>
+                <a href="#">malaria</a>
+                <a href="#">Wash</a>
+                <a href="#">Covid-19</a>
+            </section>
+
+            <section class="sub-nav">
+                <h3>Berita</h3>
+                <a href="#">Berita Hari Ini</a>
+            </section>
+            <section class="sub-nav">
+                <h3>About Us</h3>
+                <a href="#">HAKLI Papua Barat</a>
+
+            </section>
+            <section class="sub-nav">
+                <h3>Contact Us</h3>
+                <p>Wa : +08600400221</p>
+            </section>
+
+        </section>
+        <section class="logo">
+            <p>
+                ini tempat logo hakli
+            </p>
+        </section>
+    </section>
+    <section class="container-fluid copyright">
+        <p>
+            ®Copyright Himpunan Ahli Kesehatan Lingkungan Provinsi Pupua barat.
+        </p>
+
+    </section>
+
+</footer>
+
+<!-- Boostrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ front-page.php
@@ -0,0 +1,63 @@
+<main class="container-fluid main">
+
+    <!-- di bawah adalah space Dynamic -->
+    <section class="container-fluid keprofesian">
+        <h2>
+            KEPROFESIAN
+        </h2>
+        <p>
+            ini tempat keprofesian
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta placeat, quia consequuntur, unde
+            veritatis vel mollitia quisquam sapiente quo sed soluta nemo! Debitis fugit facilis error? Esse ab
+            excepturi dolores!
+        </p>
+    </section>
+
+    <!-- di atas adalah space Dynamic -->
+    <section class="container-fluid unicef">
+        <h2>
+            KEMITRAAN UNICEF
+        </h2>
+        <p>
+            ini tempat unicef
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta placeat, quia consequuntur, unde
+            veritatis vel mollitia quisquam sapiente quo sed soluta nemo! Debitis fugit facilis error? Esse ab
+            excepturi dolores!
+
+        </p>
+    </section>
+
+    <!-- di bawah adalah space Dynamic  -->
+    <section class="container-fluid agenda">
+        <h2>
+            AGENDA
+        </h2>
+        <p>
+            ini tempat agenda
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta placeat, quia consequuntur, unde
+            veritatis vel mollitia quisquam sapiente quo sed soluta nemo! Debitis fugit facilis error? Esse ab
+            excepturi dolores!
+        </p>
+    </section>
+    <section class="container-fluid berita">
+        <H2>
+            BERITA HARI INI
+        </H2>
+        <p>
+            ini tempat berita
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta placeat, quia consequuntur, unde
+            veritatis vel mollitia quisquam sapiente quo sed soluta nemo! Debitis fugit facilis error? Esse ab
+            excepturi dolores!
+        </p>
+
+    </section>
+    <!-- di atas adalah space Dynamic -->
+</main>
